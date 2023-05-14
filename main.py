1632cfe229b11c81d4401ec135af0c1b3c7c92a5
Eric Liu <twigoracle@gmail.com>
2023-05-14 13:20:47 -0700

    Changed to html -> epub

--- main.py
+++ main.py
@@ -8,9 +8,11 @@
 import bs4
 import requests 
 import re
+import textwrap
 
 import os
 import pathlib
+import subprocess
 
 ##########
 # CONFIG #
@@ -18,7 +20,7 @@ import pathlib
 
 first_chapter = "https://parahumans.wordpress.com/2011/06/11/1-1/"
 
-DATA = pathlib.Path("data/")
+DATA = pathlib.Path("data/Worm/")
 
 #############
 # FUNCTIONS #
@@ -30,6 +32,10 @@ DATA = pathlib.Path("data/")
 # CODE #
 ########
 
+################################
+# Get all the individual pages #
+################################
+
 # Create the "data/" folder if it doesn't exist
 if not os.path.exists(DATA):
     os.makedirs(DATA)
@@ -39,7 +45,8 @@ if not os.path.exists(DATA):
 arc = 1
 chapter = 0
 
-n = 0
+# Sometimes, we want to skip two lines instead of one. Use this flag
+skip_two = False
 
 # Repeat until all pages are downloaded
 next_page = first_chapter
@@ -75,13 +82,13 @@ while True:
         chapter += 1
 
     # Write the contents to a local file
-    fn = f"{str(arc).zfill(2)}.{str(chapter).zfill(2)} {title}.md"
+    fn = f"{str(arc).zfill(2)}.{str(chapter).zfill(2)} {title}.html"
     with open(DATA / fn, "w", encoding="utf8") as f:
 
         print(fn)
 
-        # Write the chapter title first
-        f.write(f"# {title}\n\n")
+        # # Write the chapter title first
+        f.write(f"<h1>{title}</h1>\n")
 
         # Filter the contents to p tags
         for p in soup.findAll("p"):
@@ -90,52 +97,14 @@ while True:
             if "Next Chapter" in p.text or "Last Chapter" in p.text:
                 continue
             elif p.text.startswith("Brief note from the author:"):
+                skip_two = True
                 continue
-            elif p.text == "■":
+            elif skip_two:
+                skip_two = False
                 continue
 
-            # Do some additonal processing on the text
-            text = ""
-            for fragment in p.contents:
-                text += str(fragment)
-
-            # Replace double spaces with single spaces
-            text = text.replace("\xa0", " ")
-            text = text.replace("  ", " ")
-
-            # Before fixing italicize and bold, escape any current * characters
-            text = text.replace("*", "\\*")
-
-            # Replace italicize
-            text = text.replace("<em> ", " *")
-            text = text.replace("<em>", "*")
-            text = text.replace(" </em>", "* ")
-            text = text.replace("</em>", "*")
-
-            # Replace bold
-            text = text.replace("<strong> ", " **")
-            text = text.replace("<strong>", "**")
-            text = text.replace(" </strong>", "** ")
-            text = text.replace("</strong>", "**")
-
-            # Replace weird indentation with more standard indentation
-            text = text.replace("■", "-")
-
-            # Sometimes, text is indented in using padding-left. Instead, we will use ">" in
-            # markdown
-            tag_style = p.attrs.get("style")
-            if tag_style is None:
-                pass
-            else:
-                if "padding-left" in tag_style:
-                    f.write("> ")
-                    text = text.replace("<br/>\n", "\n> ")
-
-            # Remove extra whitespace
-            text = text.strip()
-
-            # Write to file
-            f.write(text + "\n\n")
+            f.write(str(p))
+            f.write("\n")
 
     # Find the next page
     next_page = None
@@ -146,3 +115,35 @@ while True:
     # If no next page can be found, stop iterating
     if next_page is None:
         break
+
+#######################################
+# Combine all the pages into one EPUB #
+#######################################
+
+# Create the mega html document
+mega_html = DATA.parent / "Worm.html"
+with open(mega_html, "w", encoding="utf8") as f:
+
+    # Get all the files to combine together
+    files = DATA.glob("**/*")
+
+    # Append all the files together
+    for file in files:
+
+        # Open one of the html files
+        with open(file, "r", encoding="utf8") as to_append: 
+            f.write(to_append.read())
+            f.write("\n")
+
+# Create the metadata file
+meta = DATA.parent / "Worm_meta.txt"
+with open(meta, "w", encoding="utf8") as f:
+    f.write(textwrap.dedent("""\
+        ---
+        title: Worm
+        author: John "Wildbow" McCrae
+        language: en-US
+        ---"""))
+
+# Call pandoc to combine the data together
+subprocess.run(f"pandoc {mega_html} -o Worm.epub --metadata-file={meta}", shell=True)
