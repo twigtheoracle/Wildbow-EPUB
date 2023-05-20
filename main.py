bb94606de9b94741f4b32485e29ebbcc5609db75
Eric Liu <10490357+twigtheoracle@users.noreply.github.com>
2023-05-20 00:12:03 -0700

    Fixed failed Kindle upload
    Replaced <img> tags with alt text

--- main.py
+++ main.py
@@ -8,17 +8,23 @@
 import bs4
 import requests 
 import re
-
+import textwrap
 import os
 import pathlib
+import subprocess
 
 ##########
 # CONFIG #
 ##########
 
 first_chapter = "https://parahumans.wordpress.com/2011/06/11/1-1/"
+# first_chapter = "https://parahumans.wordpress.com/2013/03/16/interlude-19-y/"
 
-DATA = pathlib.Path("data/")
+TITLE = "Worm"
+DATA = pathlib.Path(f"data/{TITLE}")
+
+# Set to True if you want to skip already downloaded files
+skip_downloaded = False
 
 #############
 # FUNCTIONS #
@@ -30,9 +36,16 @@ DATA = pathlib.Path("data/")
 # CODE #
 ########
 
+################################
+# GET ALL THE INDIVIDUAL PAGES #
+################################
+
 # Create the "data/" folder if it doesn't exist
 if not os.path.exists(DATA):
     os.makedirs(DATA)
+
+# Sometimes, we want to skip multiple chapters in a row
+skip_n = 0
 
 # Keep track of the arc/chapter numbers. Sometimes, there are interludes in the middle of the 
 # arc making numbering somewhat inconsistent
@@ -76,66 +89,71 @@ while True:
 
     # Write the contents to a local file
     fn = f"{str(arc).zfill(2)}.{str(chapter).zfill(2)} {title}.md"
-    with open(DATA / fn, "w", encoding="utf8") as f:
 
-        print(fn)
+    # Skip if the file already exists and the setting is set
+    if (DATA / fn).is_file() and skip_downloaded:
+        print(f"Skipped {fn}")
 
-        # Write the chapter title first
-        f.write(f"# {title}\n\n")
+    # Download otherwise
+    else:
+        with open(DATA / fn, "w", encoding="utf8") as f:
 
-        # Filter the contents to p tags
-        for p in soup.findAll("p"):
+            print(fn)
 
-            # Ignore some meta text stuff
-            if "Next Chapter" in p.text or "Last Chapter" in p.text:
-                continue
-            elif p.text.startswith("Brief note from the author:"):
-                continue
-            elif p.text == "■":
-                continue
+            # Write the chapter title first
+            f.write(f"# {title}\n\n")
 
-            # Do some additonal processing on the text
-            text = ""
-            for fragment in p.contents:
-                text += str(fragment)
+            # Filter the contents to p tags
+            for p in soup.findAll("p"):
 
-            # Replace double spaces with single spaces
-            text = text.replace("\xa0", " ")
-            text = text.replace("  ", " ")
+                # Ignore some meta text stuff
+                if "Next Chapter" in p.text or "Last Chapter" in p.text:
+                    continue
+                elif p.text.startswith("Brief note from the author:"):
+                    skip_n = 1
+                    continue
+                elif skip_n > 0:
+                    skip_n -= 1
+                    continue
 
-            # Before fixing italicize and bold, escape any current * characters
-            text = text.replace("*", "\\*")
+                # Do some additonal processing on the text
+                text = ""
+                for fragment in p.contents:
 
-            # Replace italicize
-            text = text.replace("<em> ", " *")
-            text = text.replace("<em>", "*")
-            text = text.replace(" </em>", "* ")
-            text = text.replace("</em>", "*")
+                    # Replace all images with the alternate text
+                    if fragment.name == "img":
+                        print("WARNING: IMAGE FOUND")
+                        text += fragment["alt"]
 
-            # Replace bold
-            text = text.replace("<strong> ", " **")
-            text = text.replace("<strong>", "**")
-            text = text.replace(" </strong>", "** ")
-            text = text.replace("</strong>", "**")
+                    # Otherwise just add the content
+                    else:
+                        text += str(fragment)
 
-            # Replace weird indentation with more standard indentation
-            text = text.replace("■", "-")
+                # Replace double spaces with single spaces
+                text = text.replace("\xa0", " ")
+                text = text.replace("  ", " ")
 
-            # Sometimes, text is indented in using padding-left. Instead, we will use ">" in
-            # markdown
-            tag_style = p.attrs.get("style")
-            if tag_style is None:
-                pass
-            else:
-                if "padding-left" in tag_style:
-                    f.write("> ")
-                    text = text.replace("<br/>\n", "\n> ")
+                # Before fixing italicize and bold, escape any current * characters
+                text = text.replace("*", "\\*")
 
-            # Remove extra whitespace
-            text = text.strip()
+                # Replace weird indentation with more standard indentation
+                text = text.replace("■", "-")
 
-            # Write to file
-            f.write(text + "\n\n")
+                # Sometimes, text is indented in using padding-left. Instead, we will use ">" in
+                # markdown
+                tag_style = p.attrs.get("style")
+                if tag_style is None:
+                    pass
+                else:
+                    if "padding-left" in tag_style:
+                        f.write("> ")
+                        # text = text.replace("<br/>\n", "\n> ")
+
+                # Remove extra whitespace
+                text = text.strip()
+
+                # Write to file
+                f.write(text + "\n\n")
 
     # Find the next page
     next_page = None
@@ -146,3 +164,35 @@ while True:
     # If no next page can be found, stop iterating
     if next_page is None:
         break
+
+#######################################
+# Combine all the pages into one EPUB #
+#######################################
+
+# Create the mega md document
+mgra_md = DATA.parent / "Worm.md"
+with open(mgra_md, "w", encoding="utf8") as f:
+
+    # Get all the files to combine together
+    files = DATA.glob("**/*")
+
+    # Append all the files together
+    for file in files:
+
+        # Open one of the md files
+        with open(file, "r", encoding="utf8") as to_append: 
+            f.write(to_append.read())
+            f.write("\n")
+
+# Create the metadata file
+meta = DATA.parent / "Worm_meta.txt"
+with open(meta, "w", encoding="utf8") as f:
+    f.write(textwrap.dedent("""\
+        ---
+        title: Worm
+        author: John "Wildbow" McCrae
+        language: en-US
+        ---"""))
+
+# Call pandoc to combine the data together
+subprocess.run(f"pandoc {mgra_md} -o Worm.epub --metadata-file={meta}", shell=True)
